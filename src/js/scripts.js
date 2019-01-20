88b806a5f9edfab51f4bb4cd8af60a4d3411a06b
Layne Smith <laynesmith@Groots-MacBook-Pro.local>
2019-01-20 00:38:42 -0600

    Add handlebars for templating

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,17 @@
 import $ from 'jquery';
+import Handlebars from 'handlebars';
+import resume from './resume-data';
+
+const experienceSource = document.getElementById('experience-template').innerHTML;
+const experienceTemplate = Handlebars.compile(experienceSource);
+const experienceHTML = experienceTemplate(resume.experience);
+$('section.experience').append(experienceHTML);
+
+const awardSource = document.getElementById('award-template').innerHTML;
+const awardTemplate = Handlebars.compile(awardSource);
+const awardHTML = awardTemplate(resume.awards);
+$('section.awards').append(awardHTML);
+
 
 $(document).ready(() => {
   console.log('DOM Ready');
