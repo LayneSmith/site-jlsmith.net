a38ff0062754607858ba47183017e7b9eb7ed5c5
Layne Smith <laynesmith@Groots-MacBook-Pro.local>
2019-01-25 00:34:30 -0600

    Import scrollmagic

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,10 @@
 import $ from 'jquery';
+import ScrollMagic from 'scrollmagic';
+import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 import Handlebars from 'handlebars';
 import resume from './resume-data';
 
+// HANDLEBARS
 const portfolioSource = document.getElementById('portfolio-template').innerHTML;
 const portfolioTemplate = Handlebars.compile(portfolioSource);
 const portfolioHTML = portfolioTemplate(resume.portfolio);
@@ -27,6 +30,7 @@ const tapmeTemplate = Handlebars.compile(tapmeSource);
 const tapmeHTML = tapmeTemplate(resume.awards.filter(d => d.issued === 'Texas Associated Press Managing Editors'));
 $('.tapme-awards').append(tapmeHTML);
 
+// CONTROL ANIMATED GIFS ON CLICK AND HOVER
 $(document).on('mouseover', '.portfolio-piece', function () {
   const preview = $(this).data('preview');
   $(this).attr('src', `images/animations/${preview}.gif`);
@@ -44,6 +48,54 @@ $(document).on('click', '.portfolio-piece', function () {
   $(this).attr('src', newsrc);
 });
 
-$(document).ready(() => {
-  console.log('DOM Ready');
+// SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+// PLAY ANIMATION ON ROLL IN AND STOP ON ROLLOUT?
+// ANIMATE PHOTOS IN GALLERY SECTION AS COME INTO VIEW
+
+new ScrollMagic.Scene({
+  triggerElement: '.portfolio', // what will trigger scene
+  triggerHook: 0,
+})
+  .on('leave', () => {
+    $('nav').animate({
+      top: '-=40',
+    }, 500, () => {
+      $('nav').hide()
+      console.log('Slide up complete');
+    });
+  })
+  .on('enter', () => {
+    $('nav').show().animate({
+      top: '+=40',
+    }, 500, () => {
+      console.log('Slide down complete');
+    });
+  })
+  .addTo(controller);
+
+// FOR EACH SECTION SET THE CHAPTER TITLE IN NAV
+$('section').each(function () {
+  new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.5,
+  })
+    .on('enter leave', (event) => {
+      const direction = event.scrollDirection;
+      const $thisSection = $(this);
+      if(direction === 'FORWARD'){
+        const thisChapter = $thisSection.data('chapter');
+        $('nav .chapter').fadeOut('fast', function () {
+          $(this).text(thisChapter).fadeIn('fast');
+        });
+      } else {
+        const thisChapter = $thisSection.prev().data('chapter');
+        $('nav .chapter').fadeOut('fast', function () {
+          $(this).text(thisChapter).fadeIn('fast');
+        });
+      }
+    })
+    .addTo(controller);
+  
 });
